10eed30229317c014332ecf429ac22c6dd3d0a1b
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2017-07-28 07:54:53 +0200

    Modulation gets passed oscillator function

--- Sompyler/synthesizer/modulation.py
+++ Sompyler/synthesizer/modulation.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
-from Sompyler.synthesizer import SAMPLING_RATE
 from Sompyler.synthesizer.shape import Shape
 from numpy import pi, sin
 import re
@@ -9,9 +8,8 @@ import re
 class Modulation(object):
 
     def __init__(
-        self, frequency, mod_share, base_share,
+        self, frequency, mod_share, base_share, oscillator,
         phase_angle_degrees=0, overdrive=True, arg1_periods_unit=False, 
-        use_oscillator=None, envelope=None
     ):
         """ We have a constant socket and a part to modulate, the heights
             of which are given in a relation (base_share:mod_share)
@@ -31,22 +29,15 @@ class Modulation(object):
         self.mod_share  = mod_share
         self.base_share = base_share 
         self.is_dynamic = arg1_periods_unit
-        self.init_phase = phase_angle_degrees * pi / 180
+        self.init_phase = phase_angle_degrees / 360.0
         self.overdrive = overdrive # center base line
 
-        func = use_oscillator
-        if not func:
-           func = (lambda f, l, s: sin(2*pi * f * l + s))
-        if envelope:
-           inner_func = func
-           func = lambda f, l, s: envelope.render(l.size) * inner_func(f, l, s)
-
-        self.function = func
+        self.function = oscillator
 
     @classmethod
-    def from_string(cls, string, oscache):
+    def from_string(cls, string, cache, default_oscillator):
         m = re.match(
-            r"(\d+)(p)?(?:\*(\w+))?;(\d+):(\d+)(?:;([+-]\d+))?(?:,env:(.+))?",
+            r"(\d+)(p)?(?:@(\w+))?;(\d+):(\d+)([+-]\d+)?",
             string
         )
 
@@ -57,37 +48,38 @@ class Modulation(object):
                 int(m.group(1)),
                 int(m.group(4)) if m.group(4) else None,
                 int(m.group(5)) if m.group(5) else None,
-                int(m.group(6)) if m.group(6) else 0
+                None,
+                int(m.group(6)) if m.group(6) else 0,
             ]
 
             opts['arg1_periods_unit'] = bool(m.group(2))
 
             if m.group(3):
-                o = oscache[ m.group(3) ]
-                if o:
-                    opts['use_oscillator'] = o
-                else: raise Exception(
-                    "Oscillator not defined: {}".format(m.group(3))
+                pp = cache[ m.group(3) ]
+                if pp: ml[3] = pp.O
+                else: raise RuntimeError(
+                    "ProtoPartial not defined: {}".format(m.group(3))
                 )
-
-            if m.group(7):
-                opts['envelope'] = Shape.from_string( "1:" + m.group(7) )
+            else:
+                ml[3] = default_oscillator
 
             return Modulation(*ml, **opts)
 
         else:
-            raise Exception("Modulation definition syntax")
+            raise RuntimeError("Modulation definition syntax")
 
     def modulate(self, iseq, freq=None):
         b = self.base_share
         m = self.mod_share
-        f = ( freq * self.frequency if self.is_dynamic
-              else self.frequency / SAMPLING_RATE
+        f = ( freq / self.frequency if self.is_dynamic
+              else self.frequency
             )
         p = self.init_phase
-        o = (m + b) / (2*b) + 0.5 if self.overdrive else 1
+        o = (
+            (m + b) / (2*b) + 0.5 if self.overdrive else 1
+            )
         return o * (
-            m * (self.function(f, iseq, p) + 1) / 2 + b
+            m * (self.function(iseq, f, p) + 1) / 2 + b
         ) / (m + b)
 
     @classmethod
@@ -116,16 +108,21 @@ class Modulation(object):
 
         attr['overdrive'] = left.overdrive if dist < 0.5 else right.overdrive
 
-        if isinstance(left.function, 'Oscillator') and \
-           isinstance(right.function, 'Oscillator'):
+        if left.function is right.function:
+            attr['function'] = left.function
+        elif isinstance(left.function, 'Oscillator') and \
+             isinstance(right.function, 'Oscillator'):
             attr['function'] = Oscillator.weighted_average(
-                left.function, dist, right.function
-            )
+                                   left.function, dist, right.function
+                               )
         else:
-            # We do not know how to mean unknown functions, so we decide
-            # for the left when dist is below 0.5, for the right otherwise.
-            attr['function'] = left.function if dist < 0.5 else right.function
-
+            # We do not know how to mean unknown functions, so we apply both
+            # and mean the result. Please use this sparely if at all, as it is
+            # not performant.
+            attr['function'] = lambda f, l, s: avg(
+                left.function(f, l, s), right.function(f, l, s)
+            )
+            
         return Modulation(**attr)
 
 
