90260c21a427f6c9bc9f0c9359ac8d5899e17e97
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2017-11-09 21:43:48 +0100

    Changed normalization algorithm

--- Sompyler/synthesizer/__init__.py
+++ Sompyler/synthesizer/__init__.py
@@ -3,11 +3,6 @@ SAMPLING_RATE     = 44100
 CENT_PER_OCTAVE   = 1200
 BYTES_PER_CHANNEL = 2
 
-def normalize_amplitude(sound, stress=None):
-    dmin = sound.min()
-    dmax = sound.max()
-    sound -= dmin
-    sound /= max(dmax, -dmin)
-    sound -= 1
-    if stress is not None:
-        sound *= stress
+def normalize_amplitude(sound, stress=1):
+    sound /= max(sound.max(), -sound.min()) / stress
+    return sound
